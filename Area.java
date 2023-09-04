51433a34d1d1e7808e50ab71322aef6edbef5d6c
Phillip <s224278@student.dtu.dk>
2023-09-04 21:54:45 +0200

    commit2

--- Area.java
+++
@@ -1,9 +0,0 @@
-public class Area {
-    public static void main(String[] argc) {
-        int width = 3;
-        int height = 7;
-
-        int Area = width * height;
-        System.out.println("The Area is " + Area);
-    }
-}
